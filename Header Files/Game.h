4ed24e227b223493aa8596bc9a7a1e045a6b885a
coutaq <coutaq@gmail.com>
2020-11-29 01:16:58 +0300

    added the player and the game classes

---
+++ Header Files/Game.h
@@ -0,0 +1,19 @@
+//
+// Created by Michael on 29.11.2020.
+//
+
+#ifndef CPP_LAB_FOUR_GAME_H
+#define CPP_LAB_FOUR_GAME_H
+
+
+#include "Player.h"
+
+class Game {
+public:
+    //something
+private:
+    Player players[];
+};
+
+
+#endif //CPP_LAB_FOUR_GAME_H
---
+++ Header Files/Player.h
@@ -0,0 +1,14 @@
+//
+// Created by Michael on 29.11.2020.
+//
+
+#ifndef CPP_LAB_FOUR_PLAYER_H
+#define CPP_LAB_FOUR_PLAYER_H
+
+
+class Player {
+
+};
+
+
+#endif //CPP_LAB_FOUR_PLAYER_H
